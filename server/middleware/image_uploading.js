a26ddfbca3244e3b73cf1697968e0dcc690f9192
Mohamed Asaad <voldvaxe@gmail.com>
2022-06-18 20:56:41 +0200

    first commit

--- server/middleware/image_uploading.js
+++ server/middleware/image_uploading.js
@@ -29,7 +29,7 @@ const storage = multer.diskStorage({
 const upload = multer({
   storage: storage,
   limits: {
-    fileSize: 1024 * 1024 * 5,
+    fileSize: 1024 * 1024 * 20,
   },
   fileFilter: fileFilter,
 });
